48184091b7787e13a73782bf9c24ef99e6d85f52
Abhisek Das <abhid.cs@outlook.com>
2024-04-27 22:04:58 +0530

    test commit audit file

---
+++ src/services/audit.ts
@@ -0,0 +1,30 @@
+export const createAudLog = async (props: CreateAudLog) => {
+  try {
+    const { tableId, tableTitle, tableType, action, orgId } = props;
+    const session: any = await getAuthSession();
+    if (!session) {
+      return {
+        error: "user not found",
+      };
+    }
+    const user: any = await prismaDB.user.findUnique({
+      where: { email: session.user.email },
+    });
+    console.log(user, "user");
+    const audit = await prismaDB.audLog.create({
+      data: {
+        tableId,
+        tableTitle,
+        tableType,
+        action,
+        orgId,
+        userId: user.id,
+        userImage: user.image,
+        userName: user.name,
+      },
+    });
+    console.log(user, "user", audit);
+  } catch (error) {
+    console.log("audit error", error);
+  }
+};
