892111a95f480b2e52b9abe4b134502f43510455
Abhisek Das <abhid.cs@outlook.com>
2024-04-28 23:01:46 +0530

    commit

--- card.ts
+++ card.ts
@@ -48,3 +48,104 @@ export const cardCreate = async (data: {
       },
     });
 
+    await createAudLog({
+        tableId: card.id,
+        tableTitle: card.title,
+        tableType: TABLE_TYPE.CARD,
+        action: ACTION.CREATE,
+        orgId: "",
+      });
+    } catch (error) {
+      return {
+        error: "card not created",
+      };
+    }
+    revalidatePath(`/board/${boardId}`);
+    return { result: card };
+  };
+  
+  // udpate card
+  export const updateDard = async (data: z.infer<typeof UpdateCard>) => {
+    const session = await getAuthSession();
+    if (!session) {
+      return {
+        error: "user not found",
+      };
+    }
+    const { boardId, id, ...values } = data;
+    let card;
+    try {
+      card = await prismaDB.card.update({
+        where: { id },
+        data: { ...values },
+      });
+  
+      await createAudLog({
+        tableId: card.id,
+        tableTitle: card.title,
+        tableType: TABLE_TYPE.CARD,
+        action: ACTION.UPDATE,
+        orgId: "",
+      });
+    } catch (error) {
+      return {
+        error: "card not updated",
+      };
+    }
+    revalidatePath(`/board/${boardId}`);
+    return { result: card };
+  };
+  
+  // copy card
+  export const CardCopy = async (data: { id: string; boardId: string }) => {
+    const session = await getAuthSession();
+    if (!session) {
+      return {
+        error: "user not found",
+      };
+    }
+  
+    const { id, boardId } = data;
+    let card;
+  
+    try {
+      const getCard = await prismaDB.card.findUnique({ where: { id } });
+  
+      if (!getCard) {
+        return {
+          error: "card not exist",
+        };
+      }
+      const lastCard = await prismaDB.card.findFirst({
+        where: { listId: getCard?.listId },
+        orderBy: { order: "desc" },
+        select: { order: true },
+      });
+      const order = lastCard ? lastCard.order + 1 : 1;
+      console.log(lastCard);
+      card = await prismaDB.card.create({
+        data: {
+          title: `${getCard?.title} - copy`,
+          description: getCard?.description,
+          listId: getCard.listId,
+          boardId,
+          order,
+        },
+      });
+  
+      await createAudLog({
+        tableId: card.id,
+        tableTitle: card.title,
+        tableType: TABLE_TYPE.CARD,
+        action: ACTION.CREATE,
+        orgId: "",
+      });
+    } catch (error) {
+      return {
+        error: "card not created",
+      };
+    }
+    revalidatePath(`/board/${boardId}`);
+    return { result: card };
+  };
+  
