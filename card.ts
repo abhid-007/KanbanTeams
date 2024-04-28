3e2821dafef971e45cae082de5d6def9be69c765
Abhisek Das <abhid.cs@outlook.com>
2024-04-28 23:02:11 +0530

    commit

--- card.ts
+++ card.ts
@@ -148,4 +148,108 @@ export const cardCreate = async (data: {
     revalidatePath(`/board/${boardId}`);
     return { result: card };
   };
+
+// delete card
+export const CardDelete = async (data: { id: string; boardId: string }) => {
+    const session = await getAuthSession();
+    if (!session) {
+      return {
+        error: "user not found",
+      };
+    }
   
+    const { id, boardId } = data;
+    let card;
+    try {
+      card = await prismaDB.card.delete({
+        where: { id },
+      });
+    } catch (error) {
+      return {
+        error: "card not deleted",
+      };
+    }
+    await createAudLog({
+      tableId: card.id,
+      tableTitle: card.title,
+      tableType: TABLE_TYPE.CARD,
+      action: ACTION.DELETE,
+      orgId: "",
+    });
+    revalidatePath(`/board/${boardId}`);
+    return { result: card };
+  };
+  
+  // add members in card
+  export const getWithoutCardMembers = async (data: {
+    boardId: string;
+    cardId: string;
+  }) => {
+    const session = getAuthSession();
+  
+    if (!session) {
+      return {
+        error: "unauthorized",
+      };
+    }
+  
+    const { boardId, cardId } = data;
+    let users;
+    try {
+      console.log("users");
+      users = await prismaDB.user.findMany({
+        where: {
+          boards: {
+            some: { id: boardId },
+          },
+          NOT: {
+            cards: {
+              some: { id: cardId },
+            },
+          },
+        },
+      });
+    } catch (error) {
+      return {
+        error: "user not found",
+      };
+    }
+    revalidatePath(`/board/${boardId}`);
+    return { result: users };
+  };
+  
+  // add card members
+  export const addCardMember = async (data: { user: User; card: Card }) => {
+    const session = getAuthSession();
+  
+    if (!session) {
+      return {
+        error: "unauthorized",
+      };
+    }
+  
+    const { user, card } = data;
+    let updateUser, updatecard;
+    try {
+      [updateUser, updatecard] = await prismaDB.$transaction([
+        prismaDB.user.update({
+          where: { id: user.id },
+          data: {
+            cardIds: user.cardIds,
+          },
+        }),
+        prismaDB.card.update({
+          where: { id: card.id },
+          data: {
+            userIds: card.userIds,
+          },
+        }),
+      ]);
+    } catch (error) {
+      return {
+        error: "user not found",
+      };
+    }
+    revalidatePath(`/`);
+    return { result: { updateUser, updatecard } };
+  };  
